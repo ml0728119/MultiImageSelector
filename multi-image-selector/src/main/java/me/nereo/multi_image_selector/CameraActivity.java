5fe565a6348eb3581021fcfe7b1b81f93bea7b04
胡俊杰 <hujj@crop.hxqc.com>
2016-12-13 16:21:56 +0800

    增加预览

--- multi-image-selector/src/main/java/me/nereo/multi_image_selector/CameraActivity.java
+++ multi-image-selector/src/main/java/me/nereo/multi_image_selector/CameraActivity.java
@@ -14,6 +14,7 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.cameraview.CameraView;
 
 import java.io.File;
@@ -28,143 +29,183 @@ import java.io.OutputStream;
  * @since: 2016-10-31
  * Copyright:恒信汽车电子商务有限公司
  */
-public class CameraActivity extends Activity implements OnClickListener  {
-    private static final String TAG = "CameraActivity";
-    TextView mCancelView;
-    ImageView mFlashView;
-    ImageView mTakePhotoView;
-    private CameraView mCameraView;
-    public static final String PHOTO_PATH = "photo_path";
-    private static final int[] FLASH_OPTIONS = {
-            CameraView.FLASH_AUTO,
-            CameraView.FLASH_OFF,
-            CameraView.FLASH_ON,
-    };
+public class CameraActivity extends Activity implements OnClickListener {
+	private static final String TAG = "CameraActivity";
+	TextView mCancelView;
+	ImageView mFlashView;
+	ImageView mTakePhotoView;
+	ImageView mPreviewView;        //预览
+	TextView mCommitView;       //提交
+	TextView mRepetitionView;  //重拍
 
-    private static final int[] FLASH_ICONS = {
-            R.drawable.ic_flash_auto,
-            R.drawable.ic_flash_off,
-            R.drawable.ic_flash_on,
-    };
+	View mPreviewLayout;
+	View mCameraLayout;
+	private CameraView mCameraView;
+	public static final String PHOTO_PATH = "photo_path";
+	private static final int[] FLASH_OPTIONS = {
+			CameraView.FLASH_AUTO,
+			CameraView.FLASH_OFF,
+			CameraView.FLASH_ON,
+	};
 
-    private Handler mBackgroundHandler;
-    private int mCurrentFlash;
+	private static final int[] FLASH_ICONS = {
+			R.drawable.ic_flash_auto,
+			R.drawable.ic_flash_off,
+			R.drawable.ic_flash_on,
+	};
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //设置全屏无标题
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        int flag = WindowManager.LayoutParams.FLAG_FULLSCREEN;
-        setContentView(R.layout.activity_camera);
-        initView();
-    }
+	private Handler mBackgroundHandler;
+	private int mCurrentFlash;
 
-    protected void initView() {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//设置全屏无标题
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		int flag = WindowManager.LayoutParams.FLAG_FULLSCREEN;
+		setContentView(R.layout.activity_camera);
+		initView();
+	}
 
-        mCameraView = (CameraView) findViewById(R.id.camera);
-        if (mCameraView != null) {
-            mCameraView.addCallback(mCallback);
-        }
-        mFlashView = (ImageView) findViewById(R.id.flash);
-        mCancelView = (TextView) findViewById(R.id.cancel);
+	protected void initView() {
+		mPreviewLayout = findViewById(R.id.preview_layout);
+		mCameraLayout = findViewById(R.id.camera_layout);
 
-        mTakePhotoView = (ImageView) findViewById(R.id.take_photo);
-        mTakePhotoView.setOnClickListener(this);
-        mCancelView.setOnClickListener(this);
-        mFlashView.setOnClickListener(this);
-    }
-    @Override
-    public void onClick(View view) {
-        int i = view.getId();
-        if (i == R.id.take_photo) {
-            if (mCameraView != null) {
-                mCameraView.takePicture();
-            }
-        } else if (i == R.id.cancel) {
-            finish();
-        } else if (i == R.id.flash) {
-            if (mCameraView != null) {
-                mCurrentFlash = (mCurrentFlash + 1) % FLASH_OPTIONS.length;
-                mFlashView.setBackgroundResource(FLASH_ICONS[mCurrentFlash]);
-                mCameraView.setFlash(FLASH_OPTIONS[mCurrentFlash]);
-            }
-        }
-    }
+		mCameraView = (CameraView) findViewById(R.id.camera);
+		if (mCameraView != null) {
+			mCameraView.addCallback(mCallback);
+		}
+		mFlashView = (ImageView) findViewById(R.id.flash);
+		mCancelView = (TextView) findViewById(R.id.cancel);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mCameraView.start();
-    }
+		mTakePhotoView = (ImageView) findViewById(R.id.take_photo);
+		mTakePhotoView.setOnClickListener(this);
+		mCancelView.setOnClickListener(this);
+		mFlashView.setOnClickListener(this);
 
-    @Override
-    protected void onPause() {
-        mCameraView.stop();
-        super.onPause();
-    }
-    private CameraView.Callback mCallback
-            = new CameraView.Callback() {
+		mPreviewView = (ImageView) findViewById(R.id.preview);
+		mCommitView = (TextView) findViewById(R.id.commit);
+		mRepetitionView = (TextView) findViewById(R.id.repetition);
+	}
 
-        @Override
-        public void onCameraOpened(CameraView cameraView) {
-            Log.d(TAG, "onCameraOpened");
-        }
+	@Override
+	public void onClick(View view) {
+		int i = view.getId();
+		if (i == R.id.take_photo) {
+			if (mCameraView != null) {
+				mCameraView.takePicture();
+			}
+		} else if (i == R.id.cancel) {
+			finish();
+		} else if (i == R.id.flash) {
+			if (mCameraView != null) {
+				mCurrentFlash = (mCurrentFlash + 1) % FLASH_OPTIONS.length;
+				mFlashView.setBackgroundResource(FLASH_ICONS[mCurrentFlash]);
+				mCameraView.setFlash(FLASH_OPTIONS[mCurrentFlash]);
+			}
+		}
+	}
 
-        @Override
-        public void onCameraClosed(CameraView cameraView) {
-            Log.d(TAG, "onCameraClosed");
-        }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mCameraView.start();
+	}
 
-        @Override
-        public void onPictureTaken(CameraView cameraView, final byte[] data) {
-            Log.d(TAG, "onPictureTaken " + data.length);
+	@Override
+	protected void onPause() {
+		mCameraView.stop();
+		super.onPause();
+	}
 
-            getBackgroundHandler().post(new Runnable() {
-                @Override
-                public void run() {
-                    File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),
-                            "picture.jpg");
-                    Log.d(TAG, "onPictureTaken " + file.getPath());
-                    OutputStream os = null;
-                    try {
-                        os = new FileOutputStream(file);
-                        os.write(data);
-                        os.close();
-                    } catch (IOException e) {
-                        Log.w(TAG, "Cannot write to " + file, e);
-                    } finally {
-                        if (os != null) {
-                            try {
-                                os.close();
-                            } catch (IOException e) {
-                                // Ignore
-                            }
-                        }
-                    }
-                }
-            });
-        }
-    };
-    private Handler getBackgroundHandler() {
-        if (mBackgroundHandler == null) {
-            HandlerThread thread = new HandlerThread("background");
-            thread.start();
-            mBackgroundHandler = new Handler(thread.getLooper());
-        }
-        return mBackgroundHandler;
-    }
+	private CameraView.Callback mCallback
+			= new CameraView.Callback() {
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mBackgroundHandler != null) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                mBackgroundHandler.getLooper().quitSafely();
-            } else {
-                mBackgroundHandler.getLooper().quit();
-            }
-            mBackgroundHandler = null;
-        }
-    }
+		@Override
+		public void onCameraOpened(CameraView cameraView) {
+			Log.d(TAG, "onCameraOpened");
+		}
+
+		@Override
+		public void onCameraClosed(CameraView cameraView) {
+			Log.d(TAG, "onCameraClosed");
+		}
+
+		@Override
+		public void onPictureTaken(CameraView cameraView, final byte[] data) {
+			Log.d(TAG, "onPictureTaken " + data.length);
+			showPreview(data);
+		}
+	};
+
+	private void saveFile(final byte[] data) {
+		getBackgroundHandler().post(new Runnable() {
+			@Override
+			public void run() {
+				File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+						"picture.jpg");
+				Log.d(TAG, "onPictureTaken " + file.getPath());
+				OutputStream os = null;
+				try {
+					os = new FileOutputStream(file);
+					os.write(data);
+					os.close();
+				} catch (IOException e) {
+					Log.w(TAG, "Cannot write to " + file, e);
+				} finally {
+					if (os != null) {
+						try {
+							os.close();
+						} catch (IOException e) {
+							// Ignore
+						}
+					}
+				}
+			}
+		});
+	}
+
+	private void showPreview(final byte[] data) {
+		mPreviewLayout.setVisibility(View.VISIBLE);
+		mCameraLayout.setVisibility(View.GONE);
+
+		mCommitView.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				saveFile(data);
+			}
+		});
+		mRepetitionView.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mPreviewLayout.setVisibility(View.GONE);
+				mCameraLayout.setVisibility(View.VISIBLE);
+			}
+		});
+		Glide.with(this).load(data).into(mPreviewView);
+	}
+
+	private Handler getBackgroundHandler() {
+		if (mBackgroundHandler == null) {
+			HandlerThread thread = new HandlerThread("background");
+			thread.start();
+			mBackgroundHandler = new Handler(thread.getLooper());
+		}
+		return mBackgroundHandler;
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (mBackgroundHandler != null) {
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+				mBackgroundHandler.getLooper().quitSafely();
+			} else {
+				mBackgroundHandler.getLooper().quit();
+			}
+			mBackgroundHandler = null;
+		}
+	}
+
+
 }
